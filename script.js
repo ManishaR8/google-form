8be1e5e87a6cbd1cc01c62de5be32b4de39e15cd
ManishaR8 <109299159+ManishaR8@users.noreply.github.com>
2023-08-22 17:45:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.getElementById('custom-form');
+    
+//     form.addEventListener('submit', function(event) {
+//       event.preventDefault();
+  
+//       const name = document.getElementById('name').value;
+//       const email = document.getElementById('email').value;
+//       const message = document.getElementById('message').value;
+  
+//       if (name && email && message) {
+//         // Here you can perform further actions like sending data to a server
+//         console.log('Form submitted:', { name, email, message });
+//       } else {
+//         alert('Please fill in all fields.');
+//       }
+//     });
+//   });
+  
